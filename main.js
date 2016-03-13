02f356306bf694129216f8f76ccac21b847c9693
Andrew <decidoro@gmail.com>
2016-03-14 01:04:02 +0200

    create

---
+++ main.js
@@ -0,0 +1,10 @@
+
+/*window.onbeforeunload = function(){
+  return "Are you sure?";
+}
+window.onresize = function(){
+  console.log("moved")
+}
+*/
+var users = [{name: "Jason", password: "123"},{name: "Julia", password: "9999999"},{name: "Bill", password: "789654123"}];
+
